2a8bbcf923e913831e8aa3c9afb4e59c5ab657e4
Simon Walker <simon@simonwalker.tech>
2024-04-12 00:26:28 -0600

    mandel-gpu + zoom

--- src/routes/mandel-gpu/+page.svelte
+++ src/routes/mandel-gpu/+page.svelte
@@ -2,21 +2,89 @@
   import * as THREE from "three";
   import * as SC from "svelte-cubed";
   import fragmentShader from "./default.fsh?raw";
+  import { onMount } from "svelte";
 
   let width, height;
+  let mouse = [250, 200];
+  let zoom = 1;
+  let zoomMode = 1;
+
+  const handleMouse = (() => {
+    let p1 = null;
+
+    return {
+      scroll: (e) => {
+        const dy = e.deltaY;
+        zoomMode += dy / 250;
+        if (Math.abs(zoomMode) < 0.7) zoomMode = 0;
+      },
+
+      down: (e) => {
+        p1 = [e.x, e.y];
+      },
+
+      move: (e) => {
+        if (!p1) return;
+        const dx = e.x - p1[0];
+        const dy = e.y - p1[1];
+        mouse[0] -= dx / zoom;
+        mouse[1] += dy / zoom;
+        p1 = [e.x, e.y];
+      },
+
+      up: (e) => {
+        p1 = null;
+      },
+    };
+  })();
 
   $: material = new THREE.ShaderMaterial({
     uniforms: {
       size: {
         value: new THREE.Vector2(width, height),
       },
+      t: {
+        value: 0,
+      },
+      zoom: {
+        value: zoom,
+      },
+      mouse: {
+        value: mouse,
+      },
     },
     fragmentShader: fragmentShader,
-    // vertexShader: vertexShader(),
+  });
+
+  onMount(() => {
+    let t0 = Date.now() / 1000;
+    let tLast = t0;
+
+    function frame() {
+      const now = Date.now() / 1000;
+      const time = now - t0;
+      const dt = now - tLast;
+
+      material.uniforms.t.value = time;
+      zoom *= Math.pow(2, 0.3 * dt * zoomMode);
+      material.uniforms.zoom.value = zoom;
+
+      tLast = now;
+      requestAnimationFrame(frame);
+    }
+
+    frame();
   });
 </script>
 
-<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+<svelte:window
+  on:mousewheel={handleMouse.scroll}
+  on:mouseup={handleMouse.up}
+  on:mousemove={handleMouse.move}
+  on:mousedown={handleMouse.down}
+  bind:innerWidth={width}
+  bind:innerHeight={height}
+/>
 
 <SC.Canvas>
   <SC.Mesh geometry={new THREE.PlaneGeometry(2, 2)} {material} />
--- src/routes/mandel-gpu/default.fsh
+++ src/routes/mandel-gpu/default.fsh
@@ -1,6 +1,57 @@
 uniform vec2 size;
+uniform float t;
+uniform float zoom;
+uniform vec2 mouse;
+
+vec3 hsv2rgb(vec3 c) {
+  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+vec2 mandelIterSingle(vec2 z, vec2 c) {
+  return vec2(z[0]*z[0] - z[1]*z[1] + c[0],
+    2.0 * z[1] * z[0] + c[1]
+  );
+}
+
+vec2 mandel(vec2 z, vec2 c) {
+  int iMax = 1000;
+  float zMax = 2.1;
+
+  for (int i = 0; i < iMax; i++) {
+    z = mandelIterSingle(z, c);
+
+    float zMag = sqrt(z[0] * z[0] + z[1] * z[1]);
+    if (zMag >= zMax) {
+      z = mandelIterSingle(z, c); // helps decrease error
+      z = mandelIterSingle(z, c);
+      float renor = float(i) + 1.0 - log(log( zMag )) / 0.301;
+      return vec2(renor / 500.0, 1.0);
+    }
+  }
+
+  // return vec2(0,0);
+  float delta = pow((z[0] - c[0]), 2.0) + pow((z[1] - c[1]), 2.0);
+  return vec2(delta / zMax * 100.0, 0.02);
+}
 
 void main() {
-  vec2 pos = gl_FragCoord.xy / size.xy;
-  gl_FragColor = vec4(pos.x, pos.y, 1.0, 1.0);
+    float scale = 2.7;
+    vec2 offset = vec2(2, 1.5);
+
+    vec2 center = mouse / size.xy * scale;
+    vec2 pos = gl_FragCoord.xy / size.xy * scale;
+    pos = (pos - center - offset) / zoom + center;
+
+    vec2 c = pos;
+    // vec2 z0 = 0.6 * vec2(cos(t), sin(t));
+    vec2 z0 = vec2(0.0, 0.0);
+
+    vec2 result = mandel(z0, c + 0.3 * z0);
+    float hue = result.x;
+    float alt = result.y;
+    vec3 hsv = vec3(hue, 2.0 * alt, 10.0 * hue * alt);
+    vec3 rgb = hsv2rgb(hsv);
+    gl_FragColor = vec4(rgb, 1.0);
 }
